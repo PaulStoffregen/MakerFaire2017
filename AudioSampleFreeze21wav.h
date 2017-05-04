d17fac35e80233a04834b84e1a3fc89c7dabf175
Ben Davis <bendedavis@gmail.com>
2017-05-03 20:25:18 -0700

    new samples

---
+++ AudioSampleFreeze21wav.h
@@ -0,0 +1,3 @@
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleFreeze21wav[3873];
---
+++ AudioSampleFreeze22wav.h
@@ -0,0 +1,3 @@
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleFreeze22wav[3873];
